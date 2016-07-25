4eae6c311401859e1bf1c8dea04efcf2c1c8df57
Bill <wkernan@gmail.com>
2016-07-25 10:36:33 -0500

    deleted stuff

--- models/burger.js
+++ models/burger.js
@@ -1,31 +1,5 @@
 var orm = require('../config/orm.js');
 
-// var cat = {
-// 	all: function (cb) {
-// 		orm.all('cats', function (res) {
-// 			cb(res);
-// 		});
-// 	},
-// 	// cols and vals are arrays
-// 	create: function (cols, vals, cb) {
-// 		orm.create('cats', cols, vals, function (res) {
-// 			cb(res);
-// 		});
-// 	},
-// 	update: function (objColVals, condition, cb) {
-// 		orm.update('cats', objColVals, condition, function (res) {
-// 			cb(res);
-// 		});
-// 	},
-// 	delete: function(objColVals, condition, cb) {
-// 		orm.delete('cats', condition, function(res) {
-// 			cb(res);
-// 		})
-// 	}
-// };
-
-// module.exports = cat;
-
 var burger = {
 	selectAll: function(cb) {
 		orm.selectAll('burgers', function(data) {
