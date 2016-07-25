fc241e2cf6d3a9db0ce3be5083bc001cc8c89e7f
Bill <wkernan@gmail.com>
2016-07-25 11:19:02 -0500

    db stuff

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: "us-cdbr-iron-east-04.cleardb.net",
+    host: ENV['db_host'],
     port: 3306,
-    user: "b8eb4a95d2da61", 
-    password: "d98c0676", 
-    database: "heroku_b82a5a8dda5ded9"
+    user: ENV['db_user'],
+    password: ENV['db_pw'],
+    database: ENV['db']
 });
 
 module.exports = connection;
