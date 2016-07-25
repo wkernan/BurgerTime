b5e9785264edcfbe4cfecd313189149692c7fb32
Bill <wkernan@gmail.com>
2016-07-25 10:47:43 -0500

    troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "us-cdbr-iron-east-04.cleardb.net",
     port: 3306,
-    user: "root", 
-    password: "", 
-    database: "burgers_db"
+    user: "b8eb4a95d2da61", 
+    password: "d98c0676", 
+    database: "heroku_b82a5a8dda5ded9"
 });
 
 module.exports = connection;
