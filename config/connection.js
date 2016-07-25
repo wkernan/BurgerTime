c1c10b7242cebb277f0e03e561e305b45989a0ea
Bill <wkernan@gmail.com>
2016-07-25 11:36:21 -0500

    process.env

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: ENV['db_host'],
+    host: process.env.db_host,
     port: 3306,
-    user: ENV['db_user'],
-    password: ENV['db_pw'],
-    database: ENV['db']
+    user: process.env.db_user,
+    password: process.env.db_pw,
+    database: process.env.db
 });
 
 module.exports = connection;
