0073aa6d16622157c4701c76a6337d898d38f9ae
Bill <wkernan@gmail.com>
2016-07-25 19:57:32 -0500

    added db and localhost functionality

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: process.env.db_host,
+    host: process.env.db_host || "localhost",
     port: 3306,
-    user: process.env.db_user,
-    password: process.env.db_pw,
-    database: process.env.db
+    user: process.env.db_user || "root",
+    password: process.env.db_pw || "",
+    database: process.env.db || "burgers_db"
 });
 
 module.exports = connection;
